c91d4a302af194850c4302ba69bb4b202adbf868
Anne Schulz <anne.schulz@infopark.de>
2015-12-14 15:31:09 +0100

    Chef 11.10 + templates for config files

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,62 +1,77 @@
 Chef::Log.info("=== Hook: before_migrate for #{release_path}")
-
-shared_path = ::File.expand_path("#{release_path}/../../shared")
+shared_path = ::File.expand_path("#{node[:deploy][application][:deploy_to]}/shared")
 Chef::Log.info("= Shared path is #{shared_path}")
+application="dcc"
+deploy_user="deploy"
+deploy_group="www-data"
 
 Chef::Log.info("= Creating initializers Dir #{shared_path}/config/initializers")
-directory "#{shared_path}/config/initializers" do
-  mode 0755
+directory ::File.expand_path("#{shared_path}/config/initializers") do
+  mode 0770
   action :create
   recursive true
-  owner node[:opsworks][:deploy_user][:user]
-  group node[:opsworks][:deploy_user][:group]
+  owner deploy_user
+  group deploy_group
 end
 
-Chef::Log.info('= Building app config')
-
-{
-  :dcc_api_key => "Rails.configuration.dcc_api_key = '#{node[:deploy][:dcc][:api_key]}'",
-  :dcc_credentials => nil,
-  :crm_credentials => %Q@
-    Infopark::Crm.configure do |config|
-      config.url = '#{node[:deploy][:dcc][:webcrm][:url]}'
-      config.login = '#{node[:deploy][:dcc][:webcrm][:login]}'
-      config.api_key = '#{node[:deploy][:dcc][:webcrm][:api_key]}'
-    end
-  @,
-  :honeybadger => %Q@
-    Honeybadger.configure do |config|
-      config.api_key = '#{node[:deploy][:dcc][:honeybadger][:api_key]}'
-    end
-  @
-}.each do |config_name, config_content|
-  config_path = "config/initializers/#{config_name}.rb"
-  shared_config_path = ::File.join(shared_path, config_path)
-  released_config_path = ::File.join(release_path, config_path)
-
-  Chef::Log.info("= Deleting old shared app config #{shared_config_path}")
-  file shared_config_path do
+["honeybadger","crm_credentials","dcc_api_key"].each do |config_name|
+  release_config_path = "#{release_path}/config/initializers/#{config_name}.rb"
+  Chef::Log.info("= Deleting default app config #{release_config_path}")
+  file release_config_path do
     action :delete
+    only_if { ::File.exists?("#{release_config_path}") }
   end
+end
 
-  Chef::Log.info("= Deleting default app config #{released_config_path}")
-  file released_config_path do
-    action :delete
+unless node[:deploy][application][:api_key].blank?
+  Chef::Log.info("= Using template for dcc_api_key")
+  template "#{shared_path}/config/initializers/dcc_api_key.rb" do
+    local true
+    owner deploy_user
+    group deploy_group
+    mode 0664
+    variables(
+      app_name: application
+    )
+    source "#{release_path}/deploy/templates/dcc_api_key.rb.erb"
   end
+end
 
-  if config_content
-    Chef::Log.info("= Writing config file to #{shared_config_path}")
-    file shared_config_path do
-      owner node[:opsworks][:deploy_user][:user]
-      group node[:opsworks][:deploy_user][:group]
-      action :create
-      content config_content
-    end
+unless node[:deploy][application][:honeybadger][:api_key].blank?
+  Chef::Log.info("= Using template for honeybadger")
+  template "#{shared_path}/config/initializers/honeybadger.rb" do
+    local true
+    owner deploy_user
+    group deploy_group
+    mode 0664
+    variables(
+      app_name: application
+    )
+    source "#{release_path}/deploy/templates/honeybadger.rb.erb"
+  end
+end
 
-    Chef::Log.info("= Link app config #{released_config_path} to #{shared_config_path}")
-    link released_config_path do
-      to shared_config_path
-      owner node[:opsworks][:deploy_user][:user]
-    end
+unless node[:deploy][application][:webcrm][:api_key].blank?
+  Chef::Log.info("= Using template for crm_credentials")
+  template "#{shared_path}/config/initializers/crm_credentials.rb" do
+    local true
+    owner deploy_user
+    group deploy_group
+    mode 0664
+    variables(
+      app_name: application
+    )
+    source "#{release_path}/deploy/templates/crm_credentials.rb.erb"
+  end
+end
+
+["honeybadger","crm_credentials","dcc_api_key"].each do |config_name|
+  release_config_path = "#{release_path}/config/initializers/#{config_name}.rb"
+  shared_config_path = "#{shared_path}/config/initializers/#{config_name}.rb"
+  Chef::Log.info("= Link app config #{release_config_path} to #{shared_config_path}")
+  link release_config_path do
+    to shared_config_path
+    owner deploy_user
+    group deploy_group
   end
 end
